69f95c87a16d258c495f8501e871cddebca6f8b0
WinnyChang <winnypooh123@gmail.com>
2025-04-27 12:10:52 -0400

    Set up database connectivity

--- server/db.js
+++ server/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI =  "mongodb://root:<your-password>@127.0.0.1:27017";
+const mongoURI =  "mongodb://root:fjPAzRbb5KAZi7PNvyqBS03q@172.21.58.229:27017";
 
 const connectToMongo = async (retryCount) => {
     const MAX_RETRIES = 3;
