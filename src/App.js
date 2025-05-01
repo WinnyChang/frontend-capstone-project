86f37d0d30c3a2c46d0ea2c17b23edb01333b63e
WinnyChang <winnypooh123@gmail.com>
2025-04-30 20:32:56 -0400

    Create LandingPage React component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar/Navbar';
+import LandingPage from './components/LandingPage/LandingPage';
 
 function App() {
   return (
@@ -8,7 +9,7 @@ function App() {
         <BrowserRouter> {/* Set up BrowserRouter for routing */}
             <Navbar/>
             <Routes> {/* Set up the Routes for different pages */}
-
+                <Route path='/' element={<LandingPage/>}/>
             </Routes>
         </BrowserRouter>
     </div>
