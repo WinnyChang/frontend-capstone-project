7167d952e94512ecef806db036adc493db48d44e
WinnyChang <winnypooh123@gmail.com>
2025-05-02 21:14:41 -0400

    Add routes for SignUp and LogIn pages

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar/Navbar';
 import LandingPage from './components/LandingPage/LandingPage';
+import SignUp from './components/SignUp/SignUp';
+import LogIn from './components/LogIn/LogIn';
 
 function App() {
   return (
@@ -10,6 +12,8 @@ function App() {
             <Navbar/>
             <Routes> {/* Set up the Routes for different pages */}
                 <Route path='/' element={<LandingPage/>}/>
+                <Route path='/sign-up' element={<SignUp/>}/>
+                <Route path='/log-in' element={<LogIn/>}/>
             </Routes>
         </BrowserRouter>
     </div>
