676b2cd6d6738a7055819f600b3424e27893c3a7
WinnyChang <winnypooh123@gmail.com>
2025-05-09 02:30:40 -0400

    Update LandingPage

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -1,35 +1,17 @@
-import React from "react";
-import "./LandingPage.css";
+import React from 'react';
+import styles from './LandingPage.module.css';
 
 const LandingPage = () => {
   return (
-    <section className="hero-section">
-      <div>
-        <div data-aos="fade-up" className="flex-hero">
-            
-            <h1>
-              Your Health<br/>
-
-              <span className="text-gradient">
-                
-                Our Responsibility
-              </span>
-            </h1>
-              <div class="blob-cont">
-                  <div class="blue blob"></div>
-              </div>
-              <div class="blob-cont">
-                  <div class="blue1 blob"></div>
-              </div>
-            <h4>
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eaque at quae ducimus. Suscipit omnis quibusdam non cum rem voluptatem!
-            </h4>
-            <a href="#services">
-              <button class="button">Get Started</button>
-            </a>
-        </div>
+    <div className={styles.home}>
+      <div className={styles.overlay}></div>
+      <div className={styles.content}>
+        <h1 className={styles.line1}>Your Health</h1>
+        <h1 className={styles.line2}>Our Responsibility</h1>
+        <p>Access doctors anytime, anywhere.</p>
+        <button className={styles.btn}>Get Started</button>
       </div>
-    </section>
+    </div>
   );
 };
 
